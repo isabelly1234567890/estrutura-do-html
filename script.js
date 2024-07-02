0f7399854d88b72b310490f156d999ee1a6e5658
isabelly1234567890 <128759396+isabelly1234567890@users.noreply.github.com>
2024-07-02 20:09:44 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const caixaprincipal = document.querySelector
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const TextoResultado = document.querySelector(".texto-Resultado");
+
+const Perguntas = [
+    "Pergunta 1",
+    
+    "Pergunta 2"
+]
